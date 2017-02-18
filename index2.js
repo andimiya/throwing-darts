66c06c2b2fbcda1fd4f8aaa4daa23e497dea6015
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-02-18 10:50:52 -1000

    Added an index2 to solve it functionally, not done

--- index2.js
+++ index2.js
@@ -1,37 +1,31 @@
-function scoreThrows(array) {
+let array = [1, 5, 11];
+
+array.forEach(scores);
+
+function scores(e, i, arr){
   let score = 0;
   let bonus = 0;
 
-  for (var i = 0; i < array.length; i++) {
-
-    if (array[i] > 10) {
-      score += 0;
-    }
-    else if (array[i] >= 5 && array[i] <=10 ) {
-      score += 5;
-    }
-    else if (array[i] < 5) {
-      score += 10;
-      bonus++;
-    }
+  if (e > 10) {
+    score += 0;
   }
-
+  else if (e >= 5 && e <=10 ) {
+    score += 5;
+  }
+  else if (e < 5) {
+    score += 10;
+    bonus++;
+  }
   if (bonus === 4) {
-    score = score + 100;
+  score = score + 100;
   }
-
-//for each dart thrown...
-//greater than 10, then 0 points
-//if between 5 and 10, then 5 points
-//if less than 5, 10 points
-
   console.log(score);
   return score;
-
 }
 
-scoreThrows([1, 5, 11]);  // returns 15
-scoreThrows([15, 20, 30]); // return 0
-scoreThrows([1, 2, 3, 4]); // returne 140
 
-module.exports = scoreThrows;
+
+// scoreThrows([1, 5, 11]);  // returns 15
+// scoreThrows([15, 20, 30]); // return 0
+// scoreThrows([1, 2, 3, 4]); // returne 140
+
